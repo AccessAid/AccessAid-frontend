a8e19c9cb189d4f35d36f576e6e20d9258ca5049
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2023-04-22 21:17:52 +0200

    build: :wrench: Add Tailwind + MUI configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import React from 'react';
+
 function App() {
   return (
-    <div className='App'>
-      <h1>Welcome to Access Aid</h1>
+    <div className='App bg-gradient-to-r from-white via-secondary-dark to-tertiary-dark dark:from-black dark:via-secondary-light dark:to-tertiary-light'>
+      <h1 className='font-jakarta text-8xl'>Welcome to Access Aid</h1>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { store } from './store';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from '@material-tailwind/react';
 import App from './App';
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
 );
