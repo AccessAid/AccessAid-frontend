4bd061e2d4440a517b01d434d8cff6710a2a9372
Fidel Choque <82333258+fidelChoqueDev@users.noreply.github.com>
2023-05-07 03:58:43 +0200

    Add Imput con styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
     <div className='App bg-gradient-to-r from-white via-secondary-dark to-tertiary-dark dark:from-black dark:via-secondary-light dark:to-tertiary-light'>
       <h1 className='font-jakarta text-8xl'>Welcome to Access Aid</h1>
+      <Login />
     </div>
   );
 }
