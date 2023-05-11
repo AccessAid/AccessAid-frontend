f6a9f2863d42c61c801c9081b3b4ab84cc53273f
Vero Conesa <vero.gconesa@gmail.com>
2023-05-11 21:56:09 +0200

    ProfileComponent

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { RegistrationForm } from '../../components/RegistrationForm/RegistrationForm';
-
+import ProfileForm from '../../components/ProfileForm/ProfileForm.jsx';
 import './Profile.css';
 
 const Profile = () => {
   return (
     <div className='profile-container'>
-      <div className='content-container'></div>
+      <div className='content-container'>
+        <ProfileForm />
+      </div>
     </div>
   );
 };
