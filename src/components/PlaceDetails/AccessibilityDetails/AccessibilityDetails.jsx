c46aeeae8b8c3fcb15f38fbbd310686e1d07b62b
Vero Conesa <vero.gconesa@gmail.com>
2023-05-25 22:53:32 +0200

    Created AccessibilityDetails component

---
+++ src/components/PlaceDetails/AccessibilityDetails/AccessibilityDetails.jsx
@@ -0,0 +1,40 @@
+import { Chip, Tooltip, Typography } from '@material-tailwind/react';
+
+const AccessibilityDetails = () => {
+  const triggers = {
+    onMouseEnter: () => {
+      setOpenTooltip(true);
+    },
+    onMouseLeave: () => setOpenTooltip(false),
+  };
+  return (
+    <div className='align-center group mx-auto mt-8 flex flex-wrap justify-around gap-8'>
+      {accessibilityListDescription.map(({ name, description }) => (
+        <Tooltip
+          key={name}
+          content={
+            <div className='z-10 '>
+              <Typography color='white' className='font-medium'>
+                {description}
+              </Typography>
+            </div>
+          }
+          className='cursor-default'
+          animate={{
+            mount: { scale: 1, y: 0 },
+            unmount: { scale: 0, y: 25 },
+          }}
+        >
+          <Chip
+            color='indigo'
+            value={name}
+            className='flex w-20 justify-center p-1 text-xs font-normal capitalize'
+            {...triggers}
+          />
+        </Tooltip>
+      ))}
+    </div>
+  );
+};
+
+export default AccessibilityDetails;
