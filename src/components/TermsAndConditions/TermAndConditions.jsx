bd49995ee847cf47c00448e23949b3bb46e8edb5
Fidel Choque <82333258+fidelChoqueDev@users.noreply.github.com>
2023-05-26 19:14:14 +0200

    Add imports

---
+++ src/components/TermsAndConditions/TermAndConditions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { SIGNUP } from '../../config/routes';
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+  Typography,
+} from '@material-tailwind/react';
