2b770979e7d8ac70b7955c2c07159854a098089c
Vero Conesa <vero.gconesa@gmail.com>
2023-05-26 21:15:13 +0200

    Changed OpeningHours Component

--- src/components/PlaceDetails/OpeningHours/OpeningHours.jsx
+++ src/components/PlaceDetails/OpeningHours/OpeningHours.jsx
@@ -1,22 +1,45 @@
-/* eslint-disable react/prop-types */
-import { Typography } from '@material-tailwind/react';
+import {
+  Accordion,
+  AccordionBody,
+  AccordionHeader,
+  Typography,
+} from '@material-tailwind/react';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 
 const OpeningHours = ({ weekdayText }) => {
+  const [open, setOpen] = useState(1);
+  const handleOpen = (value) => {
+    setOpen(open === value ? 0 : value);
+  };
   return (
     <>
       <div>
-        <Typography color='gray'>
-          <>
-            {weekdayText.map((day) => (
-              <Typography key={day} variant='small' as='span' color='gray'>
-                {day}
-              </Typography>
-            ))}
-          </>
-        </Typography>
+        <Accordion open={open === 2}>
+          <AccordionHeader onClick={() => handleOpen(2)}>
+            Opening Hours
+          </AccordionHeader>
+          <AccordionBody>
+            <Typography color='gray'>
+              {weekdayText.map((day) => (
+                <Typography key={day} variant='small' as='span' color='gray'>
+                  {day}
+                </Typography>
+              ))}
+            </Typography>
+          </AccordionBody>
+        </Accordion>
       </div>
     </>
   );
+};
+
+OpeningHours.propTypes = {
+  weekdayText: PropTypes.arrayOf(PropTypes.string),
+};
+
+OpeningHours.defaultProps = {
+  weekdayText: [],
 };
 
 export default OpeningHours;
