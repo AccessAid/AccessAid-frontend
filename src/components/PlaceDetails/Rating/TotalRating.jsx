13d711dbdfa5952364863413a59565f89ff7f98f
Vero Conesa <vero.gconesa@gmail.com>
2023-05-28 01:35:26 +0200

    style: :lipstick:

--- src/components/PlaceDetails/Rating/TotalRating.jsx
+++ src/components/PlaceDetails/Rating/TotalRating.jsx
@@ -8,14 +8,17 @@ const TotalRating = () => {
   const totalRating = useSelector(selectTotalRatingByPlace);
   return (
     <div className='flex flex-col items-center justify-center md:flex-row lg:justify-around'>
-      <div className='flex items-center justify-start'>
+      <div className='flex flex-col items-center justify-start md:flex-row'>
         <Rating
           precision={0.25}
           size='large'
           value={totalRating}
           readOnly={true}
         />
-        <Typography color='blue-gray' className='cursor-default font-normal'>
+        <Typography
+          color='blue-gray'
+          className='ml-2 mt-2 cursor-default font-normal md:mt-0'
+        >
           {totalRating} Rating
         </Typography>
       </div>
