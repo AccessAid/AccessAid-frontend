7a1e23161ebd863950df5b61e2f68095fe05e1e8
Vero Conesa <vero.gconesa@gmail.com>
2023-05-27 23:13:48 +0200

    style: :lipstick: Fixing style

--- src/components/PlaceDetails/OpeningHours/OpeningHours.jsx
+++ src/components/PlaceDetails/OpeningHours/OpeningHours.jsx
@@ -7,6 +7,8 @@ import {
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/outline';
+
 const OpeningHours = ({ weekdayText }) => {
   const [open, setOpen] = useState(1);
   const handleOpen = (value) => {
@@ -14,13 +16,23 @@ const OpeningHours = ({ weekdayText }) => {
   };
   return (
     <>
-      <div>
+      <div className='flex justify-center'>
         <Accordion open={open === 2}>
-          <AccordionHeader onClick={() => handleOpen(2)}>
-            Opening Hours
+          <AccordionHeader
+            onClick={() => handleOpen(2)}
+            className='flex items-center justify-center '
+          >
+            <span className='flex items-center '>
+              Opening Hours
+              {open === 2 ? (
+                <ChevronUpIcon className='ml-2 h-6 w-6' />
+              ) : (
+                <ChevronDownIcon className='ml-2 h-6 w-6' />
+              )}
+            </span>
           </AccordionHeader>
-          <AccordionBody>
-            <Typography color='gray'>
+          <AccordionBody className='flex justify-center'>
+            <Typography color='gray' className='text-left'>
               {weekdayText.map((day) => (
                 <Typography key={day} variant='small' as='span' color='gray'>
                   {day}
