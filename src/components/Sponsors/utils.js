ed32d1bf1c8a7f6237acb9d006cd7cf73219b570
Vero Conesa <vero.gconesa@gmail.com>
2023-05-28 19:41:52 +0200

    feat: :memo: Added sponsors

---
+++ src/components/Sponsors/utils.js
@@ -0,0 +1,67 @@
+import BSMImage from '../../../src/assets/img/bsm-logo.png';
+import CTPImage from '../../../src/assets/img/connecting.png';
+import DomoblockImage from '../../../src/assets/img/domoblock.png';
+import DonWebImage from '../../../src/assets/img/donweb.png';
+import HackabossImage from '../../../src/assets/img/hackaboss.png';
+import MalpicadoImage from '../../../src/assets/img/malpicado.png';
+import MaltImage from '../../../src/assets/img/malt.png';
+import ProgramacionEsImage from '../../../src/assets/img/programacion_es.png';
+
+export const sponsorImages = [
+  {
+    src: ProgramacionEsImage,
+    alt: 'Transistor',
+    width: '258',
+    height: '8',
+    hasWhiteText: false,
+  },
+  {
+    src: DonWebImage,
+    alt: 'Reform',
+    width: '158',
+    height: '48',
+    hasWhiteText: false,
+  },
+  {
+    src: MalpicadoImage,
+    alt: 'Reform',
+    width: '158',
+    height: '48',
+    hasWhiteText: false,
+  },
+  {
+    src: BSMImage,
+    alt: 'Reform',
+    width: '158',
+    height: '48',
+    hasWhiteText: true,
+  },
+  {
+    src: HackabossImage,
+    alt: 'Reform',
+    width: '158',
+    height: '48',
+    hasWhiteText: false,
+  },
+  {
+    src: DomoblockImage,
+    alt: 'Reform',
+    width: '158',
+    height: '48',
+    hasWhiteText: true,
+  },
+  {
+    src: CTPImage,
+    alt: 'Reform',
+    width: '158',
+    height: '48',
+    hasWhiteText: false,
+  },
+  {
+    src: MaltImage,
+    alt: 'Reform',
+    width: '58',
+    height: '28',
+    hasWhiteText: false,
+  },
+];
