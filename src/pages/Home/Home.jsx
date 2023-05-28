6fa4bc630b4c42bdd11e861dfc245fb6bed2378f
Vero Conesa <vero.gconesa@gmail.com>
2023-05-28 21:30:12 +0200

    style: :lipstick: Changed order

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Sponsors } from '../../components/Sponsors/Sponsors';
-import { GroupCards } from '../../components/GroupCards/GroupCards';
 import { Banner } from '../../components/Banner/Banner';
-import { InfoFeatures } from '../../components/InfoFeatures/InfoFeatures';
 import { CardsInfo } from '../../components/CardsInfo/CardsInfo';
+import { GroupCards } from '../../components/GroupCards/GroupCards';
+import { InfoFeatures } from '../../components/InfoFeatures/InfoFeatures';
+import { Sponsors } from '../../components/Sponsors/Sponsors';
 
 import './Home.css';
 
@@ -11,10 +11,10 @@ const Home = () => {
   return (
     <div className='home'>
       <Banner />
-      <Sponsors />
-      <GroupCards />
       <InfoFeatures />
       <CardsInfo />
+      <Sponsors />
+      <GroupCards />
     </div>
   );
 };
