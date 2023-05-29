ef8ecbe6d4e1a69d292f4cf237e2eddf5ac88baa
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2023-05-29 13:34:48 +0200

    refactor: :recycle: remove comment section and little fix over header links

--- src/components/CommentSection/CommentForm/CommentForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './CommentForm.css';
-
-const CommentForm = () => {
-  return <div className='comment-form-container'></div>;
-};
-
-export { CommentForm };
--- src/components/CommentSection/ModalComment/ModalComment.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './ModalComment.css';
-
-const ModalComment = () => {
-  return <div className='modal-comment-container'></div>;
-};
-
-export { ModalComment };
