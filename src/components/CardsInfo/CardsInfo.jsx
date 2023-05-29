a6055a7be0b9ac2ea8193964166b925bc443e2b9
Vero Conesa <vero.gconesa@gmail.com>
2023-05-29 12:43:29 +0200

    style: :lipstick: Style

--- src/components/CardsInfo/CardsInfo.jsx
+++ src/components/CardsInfo/CardsInfo.jsx
@@ -8,7 +8,7 @@ import './CardsInfo.css';
 const CardsInfo = () => {
   return (
     <div className='section-cards-info'>
-      <div className='cards-info-container'>
+      <div className='cards-info-container -mt-14'>
         <div className='-mx-4 flex flex-wrap items-center justify-center'>
           <div className='w-full px-4 md:w-1/2 xl:w-1/3'>
             <div className='mb-10 overflow-hidden rounded-lg bg-white '>
